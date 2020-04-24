408a2311e011c3657fb19488b5586a35a7c4aade
Vivian <vlshibukawa@gmail.com>
2020-04-24 14:54:06 -0400

    add loader test

---
+++ client/src/components/loader/Loader.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Loader from "./Loader";
+
+describe("Loader component", () => {
+  it("should not render", () => {
+    const wrapper = shallow(<Loader showIf={false} />);
+    const element = wrapper.find(`div.loader`);
+    expect(element.length).toBe(0);
+  });
+
+  it("should render", () => {
+    expect.assertions(2);
+    const wrapper = shallow(<Loader showIf={true} />);
+    const element = wrapper.find(`div.loader`);
+    expect(element.length).toBe(1);
+    const elementCirular = wrapper.find(`div.loader`);
+    expect(elementCirular.length).toBe(1);
+  });
+});
