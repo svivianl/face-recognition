c632fd680c7b52f02d068926df42904cf6d2960a
Vivian <vlshibukawa@gmail.com>
2020-05-01 16:03:45 -0400

    fix Navbar test

--- client/src/components/navbar/Navbar.test.tsx
+++ client/src/components/navbar/Navbar.test.tsx
@@ -8,40 +8,38 @@ import Logo from "./components/Logo";
 import { User } from "../../types";
 
 describe("Navbar component", () => {
-  let userUseSelectorSpy: jest.SpyInstance;
+  let tokenUserSelectorSpy: jest.SpyInstance;
   let mockDispatch: any;
   let useDispatchSpy: jest.SpyInstance;
 
   beforeEach(() => {
-    userUseSelectorSpy = jest.spyOn(Redux, "useSelector");
+    tokenUserSelectorSpy = jest.spyOn(Redux, "useSelector");
     useDispatchSpy = jest.spyOn(Redux, "useDispatch");
     mockDispatch = jest.fn();
     useDispatchSpy.mockImplementation(() => (cb: Action) => cb);
   });
 
   it("should render Register and Sign In", () => {
-    userUseSelectorSpy.mockReturnValue({} as User);
+    tokenUserSelectorSpy.mockReturnValue("");
     expect.assertions(3);
     const wrapper = shallow(<Navbar />);
     expect(wrapper.find(Logo).length).toEqual(1);
     expect(
       wrapper.containsMatchingElement(
-        <Nav.Link href="/register">Register</Nav.Link>
+        <Nav.Link href="/signin">Sign In</Nav.Link>
       )
     ).toBeTruthy();
     expect(
       wrapper.containsMatchingElement(
-        <Nav.Link href="/signin">Sign In</Nav.Link>
+        <Nav.Link href="/register">Register</Nav.Link>
       )
     ).toBeTruthy();
   });
 
   it("should render Sign Out", () => {
-    userUseSelectorSpy.mockReturnValue({
-      name: "John Doe",
-      entries: 15,
-      token: "0d48c11f-a6b0-49dc-be35-1873af9f3624",
-    });
+    tokenUserSelectorSpy.mockReturnValue(
+      "0d48c11f-a6b0-49dc-be35-1873af9f3624"
+    );
 
     expect.assertions(2);
     const wrapper = shallow(<Navbar />);
